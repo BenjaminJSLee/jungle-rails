c6d4c461455a8cb9b7623301920684c2079f8f92
Benjamin Lee <benjaminjslee@gmail.com>
2021-01-08 02:50:42 +0000

    Add new sale form

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -5,6 +5,28 @@ class Admin::SalesController < ApplicationController
   end
 
   def new
+    @sale = Sale.new
   end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:sale).permit(
+        :name,
+        :starts_on,
+        :ends_on,
+        :percent_off,
+      )
+    end
 
 end
