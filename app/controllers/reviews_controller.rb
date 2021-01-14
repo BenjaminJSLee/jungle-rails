00bb188f37ff10b0a7558a8dfa7137011215df03
Benjamin Lee <benjaminjslee@gmail.com>
2021-01-14 04:49:17 +0000

    Add conditional rendering and deletion for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,37 @@
 class ReviewsController < ApplicationController
+  
+  before_action :require_login
+
   def create
-    raise "Yay, I'm here!"
+    product = Product.find_by(id: params[:product_id])
+    review = product.reviews.new(review_params)
+    review.user = current_user
+    if review.save
+      
+    end
+    redirect_to product
   end
+
+  def destroy
+    product = Product.find_by(id: params[:product_id])
+    review = Review.find params[:id]
+    if current_user == review.user
+      review.destroy
+    end
+
+    redirect_to product
+  end
+
+  private
+
+  def require_login
+    if current_user == nil
+      redirect_to :login
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
